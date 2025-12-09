b6933edaf45056d485fce856e5bbf369d9c0c495
Diksha Chottani <dikshachottani@gmail.com>
2025-12-09 20:56:25 +0000

    New Components added

---
+++ code/VR Art Gallery/Assets/art.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class art : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Art Interation component is being loaded!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
